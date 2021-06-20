2811b4b6e55ccc3f5788817e53d2e2ecc8bf7ae3
Kinoko_2k <65443692+kinoko2k@users.noreply.github.com>
2021-06-20 21:19:27 +0900

    Create mstysi.js

---
+++ discord/js/mstysi.js
@@ -0,0 +1,15 @@
+<script type="text/javascript"><!--
+function myEnter(){
+   myPassWord=prompt("wwwwのパスワードを入力してください","");
+   if ( myPassWord == "mstysi" ){
+      location.href = "secret.htm";
+   }else{
+      alert( "パスワードが違います!" );
+   }
+}
+// --></script>
+
+<form>
+ここはwwwwです→ 
+<input type="button" value="Tatsuya家へ" onclick="myEnter()">
+</form>
